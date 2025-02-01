7017887c8831988a4fb33c9ae357d3f01a65bc37
Alroy <alroypereira05@gmail.com>
2025-02-01 11:45:40 +0530

    Settings Page Done

---
+++ app/actions.ts
@@ -0,0 +1,49 @@
+"use server";
+
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+import prisma from "./lib/db";
+import { Prisma } from "@prisma/client";
+// import {  TypeOfVote } from "@prisma/client";
+// import { JSONContent } from "@tiptap/react";
+// import { revalidatePath } from "next/cache";
+
+export async function updateUsername(prevState: any, formData: FormData) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user) {
+    return redirect("/api/auth/login");
+  }
+
+  const username = formData.get("username") as string;
+
+  try {
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        userName: username,
+      },
+    });
+
+    return {
+      message: "Successfully Updated name",
+      status: "green",
+      username: updatedUser.userName, // Return the updated username
+    };
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      if (e.code === "P2002") {
+        return {
+          message: "This username is already used",
+          status: "error",
+          username: prevState.username, // Return the previous username in case of error
+        };
+      }
+    }
+
+    throw e;
+  }
+}
